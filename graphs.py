2509eed30b62ed37737e480c33ca0f6709d1f0d0
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-10-09 20:03:22 +0800

    comments, spacing and int->bool

--- graphs.py
+++ graphs.py
@@ -1,5 +1,9 @@
 import turtle
+
 print("Numbers must be less than ten")
+# mode=input("Equation (e) or coordinates (c)?")
+
+# x positive coordinates
 xp=turtle.Turtle()
 xpos=30
 while xpos <= 300:
@@ -14,6 +18,7 @@ while xpos <= 300:
     xpos+=30
 xp.goto(xpos,0)
 
+# x negative coordinates
 xn=turtle.Turtle()
 xn.left(180)
 xneg=-30
@@ -30,7 +35,7 @@ while xneg >= -300:
 xn.goto(xneg,0)
 
 
-
+# y positive coordinates
 yp=turtle.Turtle()
 yp.left(90)
 ypos=30
@@ -46,6 +51,7 @@ while ypos <= 300:
     ypos+=30
 yp.goto(0,ypos)
 
+# y negative coordinates
 yn=turtle.Turtle()
 yn.right(90)
 yneg=-30
@@ -62,17 +68,24 @@ while yneg >= -300:
 yn.goto(0,yneg)
 
 print("Done with graph")
-first_point = 0
+
+
+# drawing of points
+xpoints=[]
+ypoints=[]
+first_point = True
 ptr = turtle.Turtle()
 while True:
     pointx = float(input("Enter point x"))*30
+    xpoints.append(pointx)
     pointy = float(input("Enter point y"))*30
-    if first_point == 0:
+    ypoints.append(pointy)
+    if first_point:
         ptr.penup()
     ptr.goto(pointx,pointy)
-    if first_point == 0:
+    if first_point:
         ptr.pendown()
-        first_point += 1
+        first_point = False
     cont = input("Type 's' to stop")
     if cont == 's':
         turtle.done()
